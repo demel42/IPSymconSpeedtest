7cb55f45ad2a312721e86b217ad7aa16b473678e
demel42 <demel42@web.de>
2018-07-28 08:46:55 +0000

    Apply fixes from StyleCI

--- Speedtest/module.php
+++ Speedtest/module.php
@@ -40,7 +40,7 @@ if (!defined('IPS_STRING')) {
 
 class Speedtest extends IPSModule
 {
-	use SpeedtestCommon;
+    use SpeedtestCommon;
 
     public function Create()
     {
@@ -48,106 +48,106 @@ class Speedtest extends IPSModule
 
         $this->RegisterPropertyInteger('update_interval', '0');
 
-		$this->RegisterTimer('UpdateData', 0, 'Speedtest_UpdateData(' . $this->InstanceID . ');');
+        $this->RegisterTimer('UpdateData', 0, 'Speedtest_UpdateData(' . $this->InstanceID . ');');
 
-		$this->CreateVarProfile('Speedtest.ms', IPS_FLOAT, ' ms', 0, 0, 0, 0, '');
-		$this->CreateVarProfile('Speedtest.MBits', IPS_FLOAT, ' MBit/s', 0, 0, 0, 1, '');
+        $this->CreateVarProfile('Speedtest.ms', IPS_FLOAT, ' ms', 0, 0, 0, 0, '');
+        $this->CreateVarProfile('Speedtest.MBits', IPS_FLOAT, ' MBit/s', 0, 0, 0, 1, '');
     }
 
     public function ApplyChanges()
     {
         parent::ApplyChanges();
 
-		$vpos = 0;
-		$this->MaintainVariable('ISP', $this->Translate('ISP'), IPS_STRING, '', $vpos++, true);
-		$this->MaintainVariable('IP', $this->Translate('external IP'), IPS_STRING, '', $vpos++, true);
-		$this->MaintainVariable('Host', $this->Translate('Test-Host'), IPS_STRING, '', $vpos++, true);
-		$this->MaintainVariable('Ping', $this->Translate('Ping'), IPS_FLOAT, 'Speedtest.ms', $vpos++, true);
-		$this->MaintainVariable('Upload', $this->Translate('Upload'), IPS_FLOAT, 'Speedtest.MBits', $vpos++, true);
-		$this->MaintainVariable('Download', $this->Translate('Download'), IPS_FLOAT, 'Speedtest.MBits', $vpos++, true);
-		$this->MaintainVariable('LastStatus', $this->Translate('Last status'), IPS_INTEGER, '~UnixTimestamp', $vpos++, true);
+        $vpos = 0;
+        $this->MaintainVariable('ISP', $this->Translate('ISP'), IPS_STRING, '', $vpos++, true);
+        $this->MaintainVariable('IP', $this->Translate('external IP'), IPS_STRING, '', $vpos++, true);
+        $this->MaintainVariable('Host', $this->Translate('Test-Host'), IPS_STRING, '', $vpos++, true);
+        $this->MaintainVariable('Ping', $this->Translate('Ping'), IPS_FLOAT, 'Speedtest.ms', $vpos++, true);
+        $this->MaintainVariable('Upload', $this->Translate('Upload'), IPS_FLOAT, 'Speedtest.MBits', $vpos++, true);
+        $this->MaintainVariable('Download', $this->Translate('Download'), IPS_FLOAT, 'Speedtest.MBits', $vpos++, true);
+        $this->MaintainVariable('LastStatus', $this->Translate('Last status'), IPS_INTEGER, '~UnixTimestamp', $vpos++, true);
 
-		$this->SetStatus(102);
+        $this->SetStatus(102);
 
-		$this->SetUpdateInterval();
+        $this->SetUpdateInterval();
     }
 
-	protected function SetUpdateInterval()
-	{
-		$min = $this->ReadPropertyInteger('update_interval');
-		$msec = $min > 0 ? $min * 1000 * 60 : 0;
-		$this->SetTimerInterval('UpdateData', $msec);
-	}
+    protected function SetUpdateInterval()
+    {
+        $min = $this->ReadPropertyInteger('update_interval');
+        $msec = $min > 0 ? $min * 1000 * 60 : 0;
+        $this->SetTimerInterval('UpdateData', $msec);
+    }
 
-	public function UpdateData()
-	{
-		$time_start = microtime(true);
-		$data = exec('speedtest-cli --secure --json 2>&1', $output, $exitcode);
-		$duration = floor((microtime(true) - $time_start) * 100) / 100;
+    public function UpdateData()
+    {
+        $time_start = microtime(true);
+        $data = exec('speedtest-cli --secure --json 2>&1', $output, $exitcode);
+        $duration = floor((microtime(true) - $time_start) * 100) / 100;
 
-		if ($exitcode) {
-			$ok = false;
-			$err = $data;
-		} else {
-			$ok = true;
-			$err = '';
-		}
+        if ($exitcode) {
+            $ok = false;
+            $err = $data;
+        } else {
+            $ok = true;
+            $err = '';
+        }
 
-		if (preg_match('/speedtest-cli: error:\s(.*?)$/', $data, $r)) {
-			$err = $r[1];
-			$ok = false;
-		}
+        if (preg_match('/speedtest-cli: error:\s(.*?)$/', $data, $r)) {
+            $err = $r[1];
+            $ok = false;
+        }
 
-		$this->SendDebug(__FUNCTION__, 'duration=' . $duration . ', exitcode=' . $exitcode . ', ok=' . ($ok ? 'true' : 'false') . ', err=' . $err, 0);
+        $this->SendDebug(__FUNCTION__, 'duration=' . $duration . ', exitcode=' . $exitcode . ', ok=' . ($ok ? 'true' : 'false') . ', err=' . $err, 0);
 
-		$isp = '';
-		$ip = '';
-		$sponsor = '';
-		$ping = '';
-		$download = '';
-		$upload = '';
-				
-		if ($ok) {
-			$this->SendDebug(__FUNCTION__, 'data=' . $data, 0);
-			$jdata = json_decode($data, true);
-			if ($jdata == '') {
-				$ok = false;
-				$err = 'malformed data';
-			} else {
-				$this->SendDebug(__FUNCTION__, 'jdata=' . print_r($jdata, true), 0);
-				if (isset($jdata['client']['isp'])) {
-					$isp = $jdata['client']['isp'];
-				}
-				if (isset($jdata['client']['ip'])) {
-					$ip = $jdata['client']['ip'];
-				}
-				if (isset($jdata['server']['sponsor'])) {
-					$sponsor = $jdata['server']['sponsor'];
-				}
-				if (isset($jdata['ping'])) {
-					$ping = $jdata['ping'];
-				}
-				if (isset($jdata['download'])) {
-					$download = floor($jdata['download'] / 1024 / 1024 * 10) / 10;
-				}
-				if (isset($jdata['upload'])) {
-					$upload = floor($jdata['upload'] / 1024 / 1024 * 10) / 10;
-				}
-				$this->SendDebug(__FUNCTION__, ' ... isp=' . $isp . ' ip=' . $ip . ' sponsor=' . $sponsor . ' ping=' . $ping . ' download=' . $download . ' upload=' . $upload, 0);
-			}
-		}
+        $isp = '';
+        $ip = '';
+        $sponsor = '';
+        $ping = '';
+        $download = '';
+        $upload = '';
 
-		if ($ok) {
-			$this->SetValue('ISP', $isp);
-			$this->SetValue('IP', $ip);
-			$this->SetValue('Host', $sponsor);
-			$this->SetValue('Ping', $ping);
-			$this->SetValue('Upload', $upload);
-			$this->SetValue('Download', $download);
-		} else {
-			$msg = 'failed: exitcode=' . $exitcode . ', err=' . $err;
-			$this->LogMessage(__FUNCTION__ . ': ' . $msg, KL_WARNING);
-			$this->SendDebug(__FUNCTION__ . ': ' . $msg, 0);
-		}
-	}
+        if ($ok) {
+            $this->SendDebug(__FUNCTION__, 'data=' . $data, 0);
+            $jdata = json_decode($data, true);
+            if ($jdata == '') {
+                $ok = false;
+                $err = 'malformed data';
+            } else {
+                $this->SendDebug(__FUNCTION__, 'jdata=' . print_r($jdata, true), 0);
+                if (isset($jdata['client']['isp'])) {
+                    $isp = $jdata['client']['isp'];
+                }
+                if (isset($jdata['client']['ip'])) {
+                    $ip = $jdata['client']['ip'];
+                }
+                if (isset($jdata['server']['sponsor'])) {
+                    $sponsor = $jdata['server']['sponsor'];
+                }
+                if (isset($jdata['ping'])) {
+                    $ping = $jdata['ping'];
+                }
+                if (isset($jdata['download'])) {
+                    $download = floor($jdata['download'] / 1024 / 1024 * 10) / 10;
+                }
+                if (isset($jdata['upload'])) {
+                    $upload = floor($jdata['upload'] / 1024 / 1024 * 10) / 10;
+                }
+                $this->SendDebug(__FUNCTION__, ' ... isp=' . $isp . ' ip=' . $ip . ' sponsor=' . $sponsor . ' ping=' . $ping . ' download=' . $download . ' upload=' . $upload, 0);
+            }
+        }
+
+        if ($ok) {
+            $this->SetValue('ISP', $isp);
+            $this->SetValue('IP', $ip);
+            $this->SetValue('Host', $sponsor);
+            $this->SetValue('Ping', $ping);
+            $this->SetValue('Upload', $upload);
+            $this->SetValue('Download', $download);
+        } else {
+            $msg = 'failed: exitcode=' . $exitcode . ', err=' . $err;
+            $this->LogMessage(__FUNCTION__ . ': ' . $msg, KL_WARNING);
+            $this->SendDebug(__FUNCTION__ . ': ' . $msg, 0);
+        }
+    }
 }
